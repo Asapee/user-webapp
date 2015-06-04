2aac67166ec054af68e6d5e4e52b87c45e6b25d0
Matt Jewell <m@mattjewell.co.uk>
2015-06-04 07:40:18 +0200

    actually fix it

--- src/app/login/login.controller.js
+++ src/app/login/login.controller.js
@@ -1,29 +1,27 @@
 'use strict';
 
 angular.module('userWebapp')
-	.controller("LoginFormCtrl", [
-		function ($scope, $location, $timeout) {
-			$scope.login = function () {
-				$scope.isWorking = true;
-				var username = $scope.username;
-				var password = $scope.password;
-				Parse.User.logIn(username, password, {
-					success: function () {
-						navigator.geolocation.getCurrentPosition(function (position) {
-							var geopoint = new Parse.GeoPoint(position.coords);
-							$timeout(function () {
-								var user = Parse.User.current();
-								user.set("lastKnownLocation", geopoint);
-								user.save(); // fire and forget
-								$location.path("/home");
-							});
+	.controller("LoginFormCtrl", function ($scope, $location, $timeout) {
+		$scope.login = function () {
+			$scope.isWorking = true;
+			var username = $scope.username;
+			var password = $scope.password;
+			Parse.User.logIn(username, password, {
+				success: function () {
+					navigator.geolocation.getCurrentPosition(function (position) {
+						var geopoint = new Parse.GeoPoint(position.coords);
+						$timeout(function () {
+							var user = Parse.User.current();
+							user.set("lastKnownLocation", geopoint);
+							user.save(); // fire and forget
+							$location.path("/home");
 						});
-					},
-					error: console.error.bind(console)
-				})
-			};
-		}
-	])
+					});
+				},
+				error: console.error.bind(console)
+			})
+		};
+	})
 	.controller("RegisterFormCtrl", function ($scope, $timeout, $location) {
 		$scope.register = function () {
 			$scope.isWorking = true;
